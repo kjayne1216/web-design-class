01a3fd74515cff058abdf20ad5f8b0615b929878
kjayne1216 <kjayne1216@gmail.com>
2026-07-12 11:51:30 -0600

    Update script.js

--- Web design class/scripts/script.js
+++ Web design class/scripts/script.js
@@ -3,3 +3,16 @@ Student Name: Kelly Meadows
 File Name: script.js
 Date: "07/12/2026
 */
+
+//Hamburger menu function
+function menu() {
+    var navlinks = document.getElementById("nav-links");
+        var menuicon = document.getElementById("icon");
+        if (navlinks.style.display === "block") {
+            navlinks.style.display = "none";
+                menuicon.style.color = "#2a1f14";
+        } else {
+            navlinks.style.display = "block";
+                menuicon.style.color = "#f6eee4";
+        }
+}
